7a88e1fece402f1962d83e9f9e4cc944ddf1d9a8
SoloDev1 <solomondev27@gmail.com>
2025-12-04 16:21:01 +0100

    Changed the backend from MongoDb to Superbase

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,16 +1,22 @@
-import express from 'express';
 import { Router } from 'express';
-import { signUp, signIn, googleSignIn, appleSignIn, signOut } from '../controllers/auth.controller.js';
+import {
+    signUp,
+    signIn,
+    googleSignIn,
+    appleSignIn,
+    signOut
+} from '../controllers/auth.controller.js';
 import { authorize } from '../middleware/auth.middleware.js';
 
+const authRoute = Router();
 
-
-const authRoute = express.Router();
-
+// Public Routes
 authRoute.post("/signup", signUp);
 authRoute.post("/signin", signIn);
 authRoute.post("/google", googleSignIn);
 authRoute.post("/apple", appleSignIn);
+
+// Protected Routes
 authRoute.post("/signout", authorize, signOut);
 
 export default authRoute;
